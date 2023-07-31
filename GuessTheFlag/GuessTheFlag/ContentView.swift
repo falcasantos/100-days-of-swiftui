773f2d33da34ca29bcc52718b1e2a24a62ef9a7c
Robert Falcasantos <robert@falcasantos.com>
2023-07-31 20:18:45 +0200

    Project 2 - Guess the Flag - days 20-22

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Robert Falcasantos on 2023-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let maxQuestions = 8
+    @State private var showingScore = false
+    @State private var endingGame = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var gameResult = ""
+    @State private var gameResultMessage = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var questionsAsked = 0
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(scoreMessage)
+        }
+        .alert(gameResult, isPresented: $endingGame) {
+            Button("Start new game", action: reset)
+        } message: {
+            Text("Your final score is \(score)!")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            scoreMessage = "Your score is \(score)."
+        } else {
+            if score > 0 {
+                score -= 1
+            }
+            scoreTitle = "Wrong"
+            scoreMessage = "Wrong! That's the flag of \(countries[number])."
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        questionsAsked += 1
+        if questionsAsked == maxQuestions {
+            endingGame = true
+        }
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        askQuestion()
+        score = 0
+        questionsAsked = 0
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Robert Falcasantos on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
